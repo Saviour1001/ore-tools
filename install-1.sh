27ab3a4ca957b2f7323444915bdd60a7e692dbc5
Saviour1001 <harshghodkar@gmail.com>
2024-08-06 15:46:10 +0530

    made changes

--- install-1.sh
+++ install-1.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
  
 
-curl https://sh.rustup.rs -sSf | sh
+sudo apt-get update
 
+sudo apt-get install -y \
+    build-essential \
+    pkg-config \
+    libudev-dev llvm libclang-dev \
+    protobuf-compiler libssl-dev
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+source ~/.bashrc
 
 sh -c "$(curl -sSfL https://release.solana.com/v1.18.4/install)"
 
-solana-keygen new
+PATH="/home/codespace/.local/share/solana/install/active_release/bin:$PATH"
+
+solana-keygen new 
 
 cargo install ore-cli
 

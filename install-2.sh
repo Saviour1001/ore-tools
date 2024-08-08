b07c7a6b59723d835ea4505809e274bdcf837a59
Saviour1001 <harshghodkar@gmail.com>
2024-08-08 20:31:15 +0530

    new

--- install-2.sh
+++ install-2.sh
@@ -2,6 +2,6 @@
  
 let i=1;
 while true; do
-ore mine --rpc RPC --keypair ~/.config/solana/id.json --priority-fee 45000 --threads 8 --dynamic-fee-strategy helius --dynamic-fee-url RPC
+ore mine --rpc RPC --keypair ~/.config/solana/id.json --priority-fee 45000 --cores 8 --dynamic-fee-strategy helius --dynamic-fee-url RPC
 echo "Restarting..."
 done
